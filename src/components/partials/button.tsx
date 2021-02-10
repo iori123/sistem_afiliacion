e214468f33530af147126407f0dc066c0e90d720
renzo <renzoedward23@gmail.com>
2021-02-10 16:59:06 -0500

    addd

--- src/components/partials/button.tsx
+++ src/components/partials/button.tsx
@@ -1,10 +1,13 @@
-
+import {useState} from 'react';
 
 export const ButtonForm = (props : any) => {
-    
+    const [state, setstate] = useState({
+
+    })
+    console.log(props)
     return (
         <button 
-            
+          
             className = 'button_Form'
             type={ props.type }
         >
